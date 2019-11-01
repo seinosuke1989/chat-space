9477192ecd8de1c2d50271aff722d657a57b5667
ootaniootani <vai9984vai@yahoo.co.jp>
2019-11-01 11:53:50 +0900

    frfrfrfrfr

---
+++ spec/features/tweet_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'tweet', type: :feature do
+# このブロックの内部にscenarioを記述していく
+let(:user) { create(:user) }
+
+  scenario 'post tweet' do
+    # ログイン前には投稿ボタンがない
+    visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    find('input[name="commit"]').click
+    expect(current_path).to eq root_path
+    visit root_p
+    expect(page).to have_no_content('投稿する')
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,9 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include ControllerMacros, type: :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -61,4 +64,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  require 'capybara/rspec'
 end
