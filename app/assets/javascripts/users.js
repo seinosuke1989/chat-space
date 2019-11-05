ab9ac1ec8d5d5b4d86ffb1bf41c6bd4bacc1b102
ootaniootani <vai9984vai@yahoo.co.jp>
2019-11-05 14:55:50 +0900

    huhuhu

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,11 @@
+$(function() {
+  $("#user-search-field").on("keyup", function() {
+    let input = $("#user-search-field").val();
+    console.log(input);
+  });
+});
+
+$.ajax({
+  type: "GET",
+  
+})
